61695857601fbd890eb0b2c4e0665fab077dcd24
Théo Santos <theo.dellacqua@gmail.com>
2024-11-17 22:36:26 -0300

    Exercicio 1.2 implementado

--- exercicios_stored_procedure.sql
+++ exercicios_stored_procedure.sql
@@ -7,3 +7,20 @@ CREATE TABLE tb_log_restaurante(
     nome_log VARCHAR(300));
 
 -- em cada exercicio foi adicionado isso, para ter controle: INSERT INTO tb_log_restaurante (data_operacao, nome_log) VALUES (CURRENT_TIMESTAMP, '');
+
+
+--Exercicio 1.2
+CREATE OR REPLACE PROCEDURE sp_codigo_cliente (IN p_cod_cliente INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+    v_pedidos INT;
+BEGIN
+    SELECT COUNT(*) INTO v_pedidos FROM tb_pedido WHERE cod_cliente = p_cod_cliente;
+    RAISE NOTICE 'O cliente % tem % pedidos', p_cod_cliente, v_pedidos;
+
+    INSERT INTO tb_log_restaurante (data_operacao, nome_log) VALUES (CURRENT_TIMESTAMP, 'sp_codigo_cliente');
+END;
+$$
+
+CALL sp_codigo_cliente(1);
